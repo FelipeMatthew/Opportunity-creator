b292e4080b464e840a9e88a606447a876a9ae6fe
Gabriel Marques <gabriel.marquesbiel@hotmail.com>
2024-06-10 22:34:37 -0300

    :recycle: refactor: Final details responsive

--- assets/js/splide.js
+++ assets/js/splide.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   new Splide('#splide', {
     type   : 'loop',
-    perPage: 1,
-    gap: '1rem'
-
+    perPage: 3,
+    perMove: 1,
+    gap    : '1rem',
+    breakpoints: {
+      768: {
+        perPage: 1,
+      },
+      1024: {
+        perPage: 2, // Para desktops
+      }
+    }
   }).mount();
 });
